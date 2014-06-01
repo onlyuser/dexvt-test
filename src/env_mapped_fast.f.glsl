8d7d8e6c2ab1b1496c9924e506f92512d958912e
Jerry Chen <onlyuser@gmail.com>
2014-06-02 07:59:56 +0800

    waypoint ckin

--- src/env_mapped_fast.f.glsl
+++ src/env_mapped_fast.f.glsl
@@ -1,23 +1,10 @@
-const float AIR_REFRACTIVE_INDEX = 1.0;
-const float WATER_REFRACTIVE_INDEX = 1.333;
-
-varying vec3 fragmentNormal;
-varying vec3 cameraVector;
 uniform samplerCube env_map_texture;
-uniform float reflect_to_refract_ratio;
+uniform float       reflect_to_refract_ratio;
+
+varying vec3 reflected_flipped_cubemap_texcoord;
+varying vec3 refracted_flipped_cubemap_texcoord;
 
 void main(void) {
-    vec3 normal = normalize(fragmentNormal);
-
-    // normalize the camera direction
-    vec3 cameraDir = normalize(cameraVector);
-
-    vec3 reflected_camera_dir = reflect(-cameraDir, normal);
-    vec3 refracted_camera_dir = refract(-cameraDir, normal, AIR_REFRACTIVE_INDEX/WATER_REFRACTIVE_INDEX);
-
-    vec3 reflected_flipped_cubemap_texcoord = vec3(reflected_camera_dir.x, -reflected_camera_dir.y, reflected_camera_dir.z);
-    vec3 refracted_flipped_cubemap_texcoord = vec3(refracted_camera_dir.x, -refracted_camera_dir.y, refracted_camera_dir.z);
-
     vec4 reflected_color = textureCube(env_map_texture, reflected_flipped_cubemap_texcoord);
     vec4 refracted_color = textureCube(env_map_texture, refracted_flipped_cubemap_texcoord);
 
--- src/env_mapped_fast.v.glsl
+++ src/env_mapped_fast.v.glsl
@@ -1,17 +1,28 @@
-varying   vec3 fragmentNormal;
+const float AIR_REFRACTIVE_INDEX = 1.0;
+const float WATER_REFRACTIVE_INDEX = 1.333;
+
 attribute vec3 coord3d;
 attribute vec3 norm3d;
 uniform   mat4 mvp_xform;
 uniform   mat4 modelview_xform;
 uniform   mat4 normal_xform;
 uniform   vec3 cameraPosition;
-varying   vec3 cameraVector;
+
+varying vec3 reflected_flipped_cubemap_texcoord;
+varying vec3 refracted_flipped_cubemap_texcoord;
 
 void main(void) {
-    fragmentNormal = normalize(vec3(normal_xform*vec4(norm3d, 0)));
-
     vec3 coord3d_world = vec3(modelview_xform*vec4(coord3d, 1));
-    cameraVector = cameraPosition - coord3d_world;
+    vec3 normal_world = normalize(vec3(normal_xform*vec4(norm3d, 0)));
+
+    // normalize the camera direction
+    vec3 cameraDir = normalize(cameraPosition - coord3d_world);
+
+    vec3 reflected_camera_dir = reflect(-cameraDir, normal_world);
+    vec3 refracted_camera_dir = refract(-cameraDir, normal_world, AIR_REFRACTIVE_INDEX/WATER_REFRACTIVE_INDEX);
+
+    reflected_flipped_cubemap_texcoord = vec3(reflected_camera_dir.x, -reflected_camera_dir.y, reflected_camera_dir.z);
+    refracted_flipped_cubemap_texcoord = vec3(refracted_camera_dir.x, -refracted_camera_dir.y, refracted_camera_dir.z);
 
     gl_Position = mvp_xform*vec4(coord3d, 1);
 }
