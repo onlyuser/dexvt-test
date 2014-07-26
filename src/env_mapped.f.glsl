5084cd25a53ed1e82fd9151b0b5daa8bb7ca61a8
Jerry Chen <onlyuser@gmail.com>
2014-07-26 14:26:30 +0800

    waypoint ckin

--- src/env_mapped.f.glsl
+++ src/env_mapped.f.glsl
@@ -60,22 +60,42 @@ void intersect_ray_plane(
     orig_intersection_distance = num / denom;
 }
 
+void sample_env_map(
+        in    vec3        ray_direction,
+        in    samplerCube env_map_texture,
+        inout vec4        env_color)
+{
+    vec3 flipped_cubemap_texcoord = vec3(ray_direction.x, -ray_direction.y, ray_direction.z);
+    env_color = textureCube(env_map_texture, flipped_cubemap_texcoord);
+}
+
 void reflect_into_env_map(
-        in    vec3        camera_direction,
+        in    vec3        ray_direction,
         in    vec3        surface_normal,
         in    samplerCube env_map_texture,
         inout vec4        reflected_color)
 {
-    vec3 reflected_camera_dir = reflect(camera_direction, surface_normal);
-    vec3 reflected_flipped_cubemap_texcoord = vec3(reflected_camera_dir.x, -reflected_camera_dir.y, reflected_camera_dir.z);
-    reflected_color = textureCube(env_map_texture, reflected_flipped_cubemap_texcoord);
+    vec3 reflected_camera_dir = reflect(ray_direction, surface_normal);
+    sample_env_map(reflected_camera_dir, env_map_texture, reflected_color);
+}
+
+void refract_into_env_map(
+        in    vec3        ray_direction,
+        in    vec3        surface_normal,
+        in    float       eta,
+        in    samplerCube env_map_texture,
+        inout vec4        refracted_color,
+        inout vec3        refracted_camera_dir)
+{
+    refracted_camera_dir = refract(ray_direction, surface_normal, eta);
+    sample_env_map(refracted_camera_dir, env_map_texture, refracted_color);
 }
 
 // http://en.wikipedia.org/wiki/Chromatic_aberration
 // http://en.wikipedia.org/wiki/Dispersion_(optics)
 // http://www.opticampus.com/cecourse.php?url=chromatic_aberration
-void refract_into_env_map(
-        in    vec3        camera_direction,
+void refract_into_env_map_ex(
+        in    vec3        ray_direction,
         in    vec3        surface_normal,
         in    float       eta,
         in    float       eta_rgb_offset,
@@ -84,9 +104,9 @@ void refract_into_env_map(
         inout vec3        refracted_camera_dir)
 {
     // wiki: refraction indices of most transparent materials (e.g., air, glasses) decrease with increasing wavelength
-    vec3 refracted_camera_dirR = refract(camera_direction, surface_normal, eta-eta_rgb_offset);
-    vec3 refracted_camera_dirG = refract(camera_direction, surface_normal, eta);
-    vec3 refracted_camera_dirB = refract(camera_direction, surface_normal, eta+eta_rgb_offset);
+    vec3 refracted_camera_dirR = refract(ray_direction, surface_normal, eta-eta_rgb_offset);
+    vec3 refracted_camera_dirG = refract(ray_direction, surface_normal, eta);
+    vec3 refracted_camera_dirB = refract(ray_direction, surface_normal, eta+eta_rgb_offset);
     vec3 refracted_flipped_cubemap_texcoordR = vec3(refracted_camera_dirR.x, -refracted_camera_dirR.y, refracted_camera_dirR.z);
     vec3 refracted_flipped_cubemap_texcoordG = vec3(refracted_camera_dirG.x, -refracted_camera_dirG.y, refracted_camera_dirG.z);
     vec3 refracted_flipped_cubemap_texcoordB = vec3(refracted_camera_dirB.x, -refracted_camera_dirB.y, refracted_camera_dirB.z);
@@ -114,12 +134,24 @@ void main(void) {
     reflect_into_env_map(-camera_direction, normal, env_map_texture, reflected_color);
 
     // refraction component
+    //float eta = AIR_REFRACTIVE_INDEX/GLASS_REFRACTIVE_INDEX;
+    //vec4 refracted_color;
+    //vec3 refracted_camera_dir;
+    //refract_into_env_map(
+    //        -camera_direction,
+    //        normal,
+    //        eta,
+    //        env_map_texture,
+    //        refracted_color,
+    //        refracted_camera_dir);
+
+    // refraction component with chromatic dispersion
     float eta = AIR_REFRACTIVE_INDEX/GLASS_REFRACTIVE_INDEX;
     float eta_red = AIR_REFRACTIVE_INDEX/(GLASS_REFRACTIVE_INDEX + GLASS_REFRACTIVE_INDEX_RGB_OFFSET);
     float eta_rgb_offset = eta - eta_red;
     vec4 refracted_color;
     vec3 refracted_camera_dir;
-    refract_into_env_map(
+    refract_into_env_map_ex(
             -camera_direction,
             normal,
             eta,
@@ -141,7 +173,7 @@ void main(void) {
     float front_depth       = texture2D(front_depth_overlay_texture, overlay_texcoord).x;
     float back_depth        = texture2D(back_depth_overlay_texture, overlay_texcoord).x;
     vec3  back_normal_color = texture2D(back_normal_overlay_texture, overlay_texcoord).xyz;
-    vec3  back_normal       = back_normal_color*2 - vec3(1); // map from [0,1] to [-1,1]
+    vec3  back_normal       = normalize(back_normal_color*2 - vec3(1)); // map from [0,1] to [-1,1]
 
     float front_depth_actual = 0;
     float back_depth_actual  = 0;
@@ -160,18 +192,6 @@ void main(void) {
     vec3  dir          = refracted_camera_dir;//-camera_direction;
     vec3  plane_orig   = back_frag_position_world;
     vec3  plane_normal = back_normal;
-
-    // effect: right half window red
-    //if(overlay_texcoord.x > 0.5) {
-    //    gl_FragColor = vec4(1,0,0,0);
-    //    return;
-    //}
-
-    // effect: center big red dot (doesn't link "view_proj_xform")
-    //if(dot(normalize(plane_orig - orig), normalize(dir)) >= 0.99) {
-    //    gl_FragColor = vec4(1,0,0,0);
-    //    return;
-    //}
 
     for(int i = 0; i<NUM_NEWTONS_METHOD_ITERS; i++) {
         float orig_intersection_distance = 0;
@@ -204,27 +224,10 @@ void main(void) {
             return;
         }
 
-        /*
-        // effect: off-centered big red dot only visible at certain angle (when mesh is off-centered)
-        //         also thin red border only visible at certain angle (when mesh is off-centered)
-        if(distance(overlay_texcoord, ray_plane_isect_texcoord) < 0.25) {
-            gl_FragColor = vec4(1,0,0,0);
-            return;
-        }
-
-        // effect: gradient from red to green.
-        //         red when refracted x is bigger.
-        //         green when refracted y is bigger.
-        if(distance(overlay_texcoord, ray_plane_isect_texcoord) >= 0.25) {
-            gl_FragColor = vec4((ray_plane_isect_texcoord-overlay_texcoord+vec2(1))*0.5,0,0); // map from [-1,1] to [0,1]
-            return;
-        }
-        */
-
         float new_back_depth               = texture2D(back_depth_overlay_texture, ray_plane_isect_texcoord).x;
         vec3  new_back_frag_position_world = camera_position + normalize(ray_plane_isect-camera_position)*new_back_depth;
         vec3  new_back_normal_color        = texture2D(back_normal_overlay_texture, ray_plane_isect_texcoord).xyz;
-        vec3  new_back_normal              = new_back_normal_color*2 - vec3(1); // map from [0,1] to [-1,1]
+        vec3  new_back_normal              = normalize(new_back_normal_color*2 - vec3(1)); // map from [0,1] to [-1,1]
 
         plane_orig   = new_back_frag_position_world;
         plane_normal = new_back_normal;
