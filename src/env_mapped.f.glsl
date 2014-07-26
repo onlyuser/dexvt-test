74589d777defddaf7004618c1d2ac4e05dd94261
Jerry Chen <onlyuser@gmail.com>
2014-07-26 09:31:20 +0800

    waypoint ckin

--- src/env_mapped.f.glsl
+++ src/env_mapped.f.glsl
@@ -2,8 +2,8 @@ varying vec2      lerp_texcoord;
 uniform sampler2D normal_map_texture;
 
 const float AIR_REFRACTIVE_INDEX = 1.0;
-//const float WATER_REFRACTIVE_INDEX = 1.333;
 const float GLASS_REFRACTIVE_INDEX = 1.5;
+const float GLASS_REFRACTIVE_INDEX_RGB_OFFSET = 0.01;
 
 const float BUMP_FACTOR = 0.001;
 const float FRESNEL_REFLECTANCE_SHARPNESS = 2.0;
@@ -60,6 +60,41 @@ void intersect_ray_plane(
     orig_intersection_distance = num / denom;
 }
 
+void reflect_into_env_map(
+        in    vec3        camera_direction,
+        in    vec3        surface_normal,
+        in    samplerCube env_map_texture,
+        inout vec4        reflected_color)
+{
+    vec3 reflected_camera_dir = reflect(camera_direction, surface_normal);
+    vec3 reflected_flipped_cubemap_texcoord = vec3(reflected_camera_dir.x, -reflected_camera_dir.y, reflected_camera_dir.z);
+    reflected_color = textureCube(env_map_texture, reflected_flipped_cubemap_texcoord);
+}
+
+void refract_into_env_map(
+        in    vec3        camera_direction,
+        in    vec3        surface_normal,
+        in    float       eta,
+        in    float       eta_rgb_offset,
+        in    samplerCube env_map_texture,
+        inout vec4        refracted_color,
+        inout vec3        refracted_camera_dir)
+{
+    // refraction component + chromatic dispersion
+    // wiki: refraction indices of most transparent materials (e.g., air, glasses) decrease with increasing wavelength
+    vec3 refracted_camera_dirR = refract(camera_direction, surface_normal, eta-eta_rgb_offset);
+    vec3 refracted_camera_dirG = refract(camera_direction, surface_normal, eta);
+    vec3 refracted_camera_dirB = refract(camera_direction, surface_normal, eta+eta_rgb_offset);
+    vec3 refracted_flipped_cubemap_texcoordR = vec3(refracted_camera_dirR.x, -refracted_camera_dirR.y, refracted_camera_dirR.z);
+    vec3 refracted_flipped_cubemap_texcoordG = vec3(refracted_camera_dirG.x, -refracted_camera_dirG.y, refracted_camera_dirG.z);
+    vec3 refracted_flipped_cubemap_texcoordB = vec3(refracted_camera_dirB.x, -refracted_camera_dirB.y, refracted_camera_dirB.z);
+    refracted_color.r = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordR).r;
+    refracted_color.g = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordG).g;
+    refracted_color.b = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordB).b;
+    refracted_color.a = 1;
+    refracted_camera_dir = refracted_camera_dirG;
+}
+
 // http://en.wikipedia.org/wiki/Chromatic_aberration
 // http://en.wikipedia.org/wiki/Dispersion_(optics)
 // http://www.opticampus.com/cecourse.php?url=chromatic_aberration
@@ -76,24 +111,23 @@ void main(void) {
     vec3 normal = normalize(lerp_tbn_xform*normal_surface);
 
     // reflection component
-    vec3 reflected_camera_dir = reflect(-camera_direction, normal);
-    vec3 reflected_flipped_cubemap_texcoord = vec3(reflected_camera_dir.x, -reflected_camera_dir.y, reflected_camera_dir.z);
-    vec4 reflected_color = textureCube(env_map_texture, reflected_flipped_cubemap_texcoord);
+    vec4 reflected_color;
+    reflect_into_env_map(-camera_direction, normal, env_map_texture, reflected_color);
 
-    // refraction component + chromatic dispersion
-    // wiki: refraction indices of most transparent materials (e.g., air, glasses) decrease with increasing wavelength
+    // refraction component
     float eta = AIR_REFRACTIVE_INDEX/GLASS_REFRACTIVE_INDEX;
-    vec3 refracted_camera_dirR = refract(-camera_direction, normal, eta-0.01);
-    vec3 refracted_camera_dirG = refract(-camera_direction, normal, eta);
-    vec3 refracted_camera_dirB = refract(-camera_direction, normal, eta+0.01);
-    vec3 refracted_flipped_cubemap_texcoordR = vec3(refracted_camera_dirR.x, -refracted_camera_dirR.y, refracted_camera_dirR.z);
-    vec3 refracted_flipped_cubemap_texcoordG = vec3(refracted_camera_dirG.x, -refracted_camera_dirG.y, refracted_camera_dirG.z);
-    vec3 refracted_flipped_cubemap_texcoordB = vec3(refracted_camera_dirB.x, -refracted_camera_dirB.y, refracted_camera_dirB.z);
+    float eta_red = AIR_REFRACTIVE_INDEX/(GLASS_REFRACTIVE_INDEX + GLASS_REFRACTIVE_INDEX_RGB_OFFSET);
+    float eta_rgb_offset = eta - eta_red;
     vec4 refracted_color;
-    refracted_color.r = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordR).r;
-    refracted_color.g = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordG).g;
-    refracted_color.b = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordB).b;
-    refracted_color.a = 1;
+    vec3 refracted_camera_dir;
+    refract_into_env_map(
+            -camera_direction,
+            normal,
+            eta,
+            eta_rgb_offset,
+            env_map_texture,
+            refracted_color,
+            refracted_camera_dir);
 
     // fresnel component
     float one_minus_dot = 1 - clamp(dot(camera_direction, normal), 0, 1);
@@ -119,12 +153,12 @@ void main(void) {
     float frag_thickness = back_depth_actual - front_depth_actual;
 
     vec3 back_frag_position_world = camera_position - camera_direction*back_depth_actual;
-    //vec3 ray_plane_isect = lerp_vertex_position_world + refracted_camera_dirG*???;
+    //vec3 ray_plane_isect = lerp_vertex_position_world + refracted_camera_dir*???;
 
     // apply newton's method to find backface intersection with refracted ray from camera
 
     vec3  orig         = lerp_vertex_position_world;
-    vec3  dir          = refracted_camera_dirG;//-camera_direction;
+    vec3  dir          = refracted_camera_dir;//-camera_direction;
     vec3  plane_orig   = back_frag_position_world;
     vec3  plane_normal = back_normal;
 
