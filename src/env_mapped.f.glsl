55e9ffa271ccf346016adf36a14d68df10c03319
Jerry Chen <onlyuser@gmail.com>
2014-07-26 08:32:56 +0800

    waypoint ckin

--- src/env_mapped.f.glsl
+++ src/env_mapped.f.glsl
@@ -61,8 +61,9 @@ void intersect_ray_plane(
 
 void main(void) {
     vec3 camera_direction = normalize(lerp_camera_vector);
-    //vec3 camera_vector = camera_position - lerp_vertex_position_world;
-    //vec3 camera_direction = normalize(camera_vector);
+
+    // another way to get camera direction
+    //vec3 camera_direction = normalize(camera_position - lerp_vertex_position_world);
 
     vec2 flipped_texcoord = vec2(lerp_texcoord.x, 1 - lerp_texcoord.y);
 
@@ -70,25 +71,26 @@ void main(void) {
             mix(vec3(0, 0, 1), normalize(vec3(texture2D(normal_map_texture, flipped_texcoord))), BUMP_FACTOR);
     vec3 normal = normalize(lerp_tbn_xform*normal_surface);
 
+    // reflection component
     vec3 reflected_camera_dir = reflect(-camera_direction, normal);
+    vec3 reflected_flipped_cubemap_texcoord = vec3(reflected_camera_dir.x, -reflected_camera_dir.y, reflected_camera_dir.z);
+    vec4 reflected_color = textureCube(env_map_texture, reflected_flipped_cubemap_texcoord);
+
+    // refraction component + chromatic dispersion
     float etaR = AIR_REFRACTIVE_INDEX/WATER_REFRACTIVE_INDEX;
     vec3 refracted_camera_dirR = refract(-camera_direction, normal, etaR);
     vec3 refracted_camera_dirG = refract(-camera_direction, normal, etaR+0.008);
     vec3 refracted_camera_dirB = refract(-camera_direction, normal, etaR+0.016);
-
-    vec3 reflected_flipped_cubemap_texcoord  = vec3(reflected_camera_dir.x,  -reflected_camera_dir.y,  reflected_camera_dir.z);
     vec3 refracted_flipped_cubemap_texcoordR = vec3(refracted_camera_dirR.x, -refracted_camera_dirR.y, refracted_camera_dirR.z);
     vec3 refracted_flipped_cubemap_texcoordG = vec3(refracted_camera_dirG.x, -refracted_camera_dirG.y, refracted_camera_dirG.z);
     vec3 refracted_flipped_cubemap_texcoordB = vec3(refracted_camera_dirB.x, -refracted_camera_dirB.y, refracted_camera_dirB.z);
-
-    vec4 reflected_color = textureCube(env_map_texture, reflected_flipped_cubemap_texcoord);
-
     vec4 refracted_color;
     refracted_color.r = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordR).r;
     refracted_color.g = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordG).g;
     refracted_color.b = textureCube(env_map_texture, refracted_flipped_cubemap_texcoordB).b;
     refracted_color.a = 1;
 
+    // fresnel component
     float one_minus_dot = 1 - clamp(dot(camera_direction, normal), 0, 1);
     float fresnel_reflectance_attenuation = pow(one_minus_dot, FRESNEL_REFLECTANCE_SHARPNESS);
 
