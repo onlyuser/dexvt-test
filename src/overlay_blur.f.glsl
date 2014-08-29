0f344c4d0da164975081d72b1a14badd52ece57a
Jerry Chen <onlyuser@gmail.com>
2014-08-30 02:30:08 +0800

    waypoint ckin

--- src/overlay_blur.f.glsl
+++ src/overlay_blur.f.glsl
@@ -1,16 +1,24 @@
 varying vec2      lerp_texcoord;
 uniform sampler2D color_texture;
-varying vec2      pixel_offset;
+varying vec2      lerp_pixel_offset;
 
 void main(void) {
-    vec2 up    = vec2(lerp_texcoord.x, lerp_texcoord.y + pixel_offset.y);
-    vec2 down  = vec2(lerp_texcoord.x, lerp_texcoord.y - pixel_offset.y);
-    vec2 left  = vec2(lerp_texcoord.x - pixel_offset.x, lerp_texcoord.y);
-    vec2 right = vec2(lerp_texcoord.x + pixel_offset.x, lerp_texcoord.y);
+    vec2 up     = vec2(lerp_texcoord.x, lerp_texcoord.y + lerp_pixel_offset.y);
+    vec2 up2    = vec2(lerp_texcoord.x, lerp_texcoord.y + lerp_pixel_offset.y*2);
+    vec2 down   = vec2(lerp_texcoord.x, lerp_texcoord.y - lerp_pixel_offset.y);
+    vec2 down2  = vec2(lerp_texcoord.x, lerp_texcoord.y - lerp_pixel_offset.y*2);
+    vec2 left   = vec2(lerp_texcoord.x - lerp_pixel_offset.x,   lerp_texcoord.y);
+    vec2 left2  = vec2(lerp_texcoord.x - lerp_pixel_offset.x*2, lerp_texcoord.y);
+    vec2 right  = vec2(lerp_texcoord.x + lerp_pixel_offset.x,   lerp_texcoord.y);
+    vec2 right2 = vec2(lerp_texcoord.x + lerp_pixel_offset.x*2, lerp_texcoord.y);
     gl_FragColor =
-            (texture2D(color_texture, lerp_texcoord) +
-             texture2D(color_texture, up) +
-             texture2D(color_texture, down) +
-             texture2D(color_texture, left) +
-             texture2D(color_texture, right))/5;
+            (texture2D(color_texture, lerp_texcoord)*6 +
+             texture2D(color_texture, up)*4 +
+             texture2D(color_texture, up2) +
+             texture2D(color_texture, down)*4 +
+             texture2D(color_texture, down2) +
+             texture2D(color_texture, left)*4 +
+             texture2D(color_texture, left2) +
+             texture2D(color_texture, right)*4 +
+             texture2D(color_texture, right2))/(6+(1+4)*4);
 }
--- src/overlay_blur.v.glsl
+++ src/overlay_blur.v.glsl
@@ -1,9 +1,11 @@
 varying vec2 lerp_texcoord;
 uniform vec2 viewport_dim;
-varying vec2 pixel_offset;
+varying vec2 lerp_pixel_offset;
+
+const int SAMPLE_DISTANCE = 5;
 
 void main(void) {
     gl_Position = gl_Vertex;
     lerp_texcoord = (vec2(gl_Vertex) + vec2(1))*0.5; // map from [-1,1] to [0,1];
-    pixel_offset = vec2(1/(viewport_dim.x*0.1), 1/(viewport_dim.y*0.1));
+    lerp_pixel_offset = vec2(1/viewport_dim.x, 1/viewport_dim.y)*SAMPLE_DISTANCE;
 }
