bbf40d130f4dac73d58f43e8971fba2f2136398e
Jerry Chen <onlyuser@gmail.com>
2014-09-02 00:28:00 +0800

    waypoint ckin

--- src/overlay_blur.f.glsl
+++ src/overlay_blur.f.glsl
@@ -5,13 +5,13 @@ uniform float     bloom_kernel[25];
 
 void main(void) {
     vec4 sum_color;
-    for(int i = 0; i<5; i++) {
-        for(int j = 0; j<5; j++) {
-            ivec2 kernel_offset = ivec2(i - 2, j - 2);
+    for(int y = 0; y<5; y++) {
+        for(int x = 0; x<5; x++) {
+            ivec2 kernel_offset = ivec2(x - 2, y - 2);
             vec2 sample_coord = vec2(lerp_texcoord.x + lerp_pixel_offset.x*kernel_offset.x,
                                      lerp_texcoord.y + lerp_pixel_offset.y*kernel_offset.y);
             vec4 sample_color = texture2D(color_texture, sample_coord);
-            float sample_weight = bloom_kernel[i*5 + j];
+            float sample_weight = bloom_kernel[y*5 + x];
             sum_color += clamp(sample_color*sample_weight, 0, 1);
         }
     }
