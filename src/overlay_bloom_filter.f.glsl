8e7f2dc9c259860fa6a916312c17190546015e93
Jerry Chen <onlyuser@gmail.com>
2014-09-08 15:58:48 +0800

    waypoint ckin

--- src/overlay_bloom_filter.f.glsl
+++ src/overlay_bloom_filter.f.glsl
@@ -1,9 +1,10 @@
+const int   BLOOM_KERNEL_SIZE = 5;
+const float GLOW_CUTOFF_THRESHOLD = 0.75;
+
 varying vec2      lerp_texcoord;
 uniform sampler2D color_texture;
 varying vec2      lerp_pixel_offset;
-uniform float     bloom_kernel[5];
-
-const float GLOW_CUTOFF_THRESHOLD = 0.75;
+uniform float     bloom_kernel[BLOOM_KERNEL_SIZE];
 
 void sum_components(in vec4 color, out float sum) {
     sum = color.r + color.g + color.b;
@@ -11,7 +12,7 @@ void sum_components(in vec4 color, out float sum) {
 
 void main(void) {
     vec4 sum_color;
-    for(int i = 0; i<5; i++) {
+    for(int i = 0; i < BLOOM_KERNEL_SIZE; i++) {
         float sample_weight = bloom_kernel[i];
         ivec2 hblur_kernel_offset = ivec2(i - 2, 0);
         ivec2 vblur_kernel_offset = ivec2(0, i - 2);
--- src/overlay_max.f.glsl
+++ src/overlay_max.f.glsl
@@ -4,4 +4,5 @@ uniform sampler2D color_texture2;
 
 void main(void) {
     gl_FragColor = max(texture2D(color_texture, lerp_texcoord), texture2D(color_texture2, lerp_texcoord));
+    //gl_FragColor = texture2D(color_texture, lerp_texcoord)*0.001 + texture2D(color_texture2, lerp_texcoord);
 }
