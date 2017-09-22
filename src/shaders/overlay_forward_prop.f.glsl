e430b8ddf7dc9a1a51746465ba626f32ee851ef4
onlyuser <onlyuser@gmail.com>
2017-09-22 09:32:21 +0800

    waypoint ckin

--- src/shaders/overlay_forward_prop.f.glsl
+++ src/shaders/overlay_forward_prop.f.glsl
@@ -2,6 +2,20 @@ uniform sampler2D in_texture;
 //uniform sampler2D weight_texture;
 varying vec2 lerp_texcoord;
 
+float rgb_to_float(in vec4  value,
+                   in float min_float,
+                   in float max_float)
+{
+    return min_float + (value.r * 65536 + value.g * 256 + value.b) * (max_float - min_float);
+}
+
+vec4 float_to_rgb(in float value,
+                  in float min_float,
+                  in float max_float)
+{
+    float x = (value - min_float) / (max_float - min_float);
+}
+
 void main(void) {
     // draw thin red line
     ivec2 pos = ivec2(gl_FragCoord.xy);
